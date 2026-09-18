2391d0f32ea4bc6bd9b70d41a4d6856ba029abe6
RikuSpl <kiituriii@proton.me>
2026-09-18 22:17:05 +0300

    added functionality for adding todo items, styling details

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -1,14 +1,26 @@
 import { useState } from 'react'
 import './components.css'
-import { Item } from './Item'
+
+type TodoItem = {
+    id: string
+    title: string
+}
 
 export function Card() {
 
+
     const [inputValue, setInputValue] = useState("")
-    function handleEvent(e) {
-        return setInputValue(e.target.value);
+    const [todoItem, setTodoItem] = useState<TodoItem[]>([])
+    
+    function handleEvent(e: React.ChangeEvent<HTMLInputElement>) {
+        setInputValue(e.target.value);
     }
 
+    function handleSubmitEvent(e: React.SubmitEvent<HTMLFormElement>) {
+        e.preventDefault()
+        setTodoItem(currentTodo => [...currentTodo, { id: crypto.randomUUID(), title: inputValue },]) 
+        
+    }
 
     return  ( 
 
@@ -16,15 +28,27 @@ export function Card() {
             <h1>Your To-do List</h1>
             <div className="card">
                 <div className="item-add">
-                    <form>
-                        <input value={inputValue} onChange={handleEvent} placeholder='Add new todo item ...'/>
+                    <form onSubmit = {handleSubmitEvent}>
+                        <input id="todoInput"value={inputValue} onChange={handleEvent} placeholder='Add new todo item ...' maxLength={25} />
                         <button>Submit</button>
                     </form>
                     <div className="todo-list">
                         <p className="title">to-do list</p>
                         <ul>
-                            <Item />
-                            <Item />
+                            {todoItem.length === 0 ? (
+                                <li id="emptyList">Add a new todo into your list above!</li>
+                            ) : (
+                                todoItem.map(item => (
+                                <li key={item.id}>
+                                    <span className="item-name">{item.title}</span>
+                                    <div className="item-buttons">
+                                        <button className="button-done">Done</button>
+                                        <button className="button-remove">Remove</button>
+                                    </div>
+                                </li>
+                            ))
+                            )}
+                                                        
                         </ul>
                     </div>
                 </div>
